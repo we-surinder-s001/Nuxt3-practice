dddf945d6bed355e055d094b2cb52057c42b557d
Surinder Singh <surinder-s001@webreinvent.com>
2024-01-10 12:56:59 +0530

    create the sigle product page and implemented useFetch

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -2,11 +2,23 @@
 definePageMeta({
   layout: "products-layout"
 })
-const {id} = useRoute().params
+const {id} = useRoute().params;
+
+const uri = `https://fakestoreapi.com/products/${id}`;
+
+const {data: product} = await useFetch(uri, {key: id});
+
+
 </script>
 
 <template>
-  <h1>
-    {{ id }}
-  </h1>
+  <div>
+    <img :src="product.image" :alt="product.title">
+    <br>
+    <h2>{{ product.title }}</h2>
+    <br>
+    <p>description : {{ product.description }}</p>
+    <br>
+    <p>Price : {{ product.price }}</p>
+  </div>
 </template>
